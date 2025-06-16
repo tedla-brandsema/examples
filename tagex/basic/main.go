732a45dd69e08b3ac0511bfb75bbecf8dc49d854
Tedla Brandsema <git@tedla.dev>
2025-06-16 15:45:22 +0200

    ProcessStruct needs a pointer to a struct. Changed documentation on the EmailDirective.

--- tagex/basic/main.go
+++ tagex/basic/main.go
@@ -6,22 +6,22 @@ import (
 	"net/mail"
 )
 
-// EmailDirective implements the tagex.Directive[T any] interface by defining
-// both the "Name() string" and "Handle(val T) error" methods.
+// EmailDirective implements the tagex.Directive interface.
 type EmailDirective struct{}
 
+// Name returns the directive name. This string is used in when applying the directive to a field via a tag.
 func (d *EmailDirective) Name() string {
 	return "email"
 }
 
+// Mode tells the tagex library whether this directive simply evaluates the field value or mutates it.
 func (d *EmailDirective) Mode() tagex.DirectiveMode {
 	return tagex.EvalMode
 }
 
-// Even though tagex.Directive[T any] is generic, your implementation of it can be explicit.
-// Here Handle() explicitly is of type "int", which makes our "EmailDirective" explicitly of type "string".
-// This means we can use our EmailDirective only on fields of type "string".
-func (v *EmailDirective) Handle(val string) (string, error) {
+// Handle is where the actual work of the directive takes place. Here you can evaluate the field value or mutate it,
+// depending on what value the Mode method returns
+func (d *EmailDirective) Handle(val string) (string, error) {
 	_, err := mail.ParseAddress(val)
 	return val, err
 }
@@ -67,7 +67,7 @@ func main() {
 
 	// Check our contacts by calling "ProcessStruct" on our tag
 	for _, contact := range contacts {
-		if ok, err := checkTag.ProcessStruct(contact); !ok {
+		if ok, err := checkTag.ProcessStruct(&contact); !ok {
 			fmt.Printf("Invalid email %q: %v\n", contact.Email, err)
 			continue
 		}
