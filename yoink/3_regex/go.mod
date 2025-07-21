c9716c810036d16cdf713fdf0f2c8c63ed964a16
Tedla Brandsema <git@tedla.dev>
2025-07-21 13:58:01 +0200

    Yoink example 3_regex

---
+++ yoink/3_regex/go.mod
@@ -0,0 +1,3 @@
+module github.com/tedla-brandsema/examples/yoink/3_regex
+
+go 1.24.4
---
+++ yoink/3_regex/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/tedla-brandsema/yoink"
+	"os"
+)
+
+func main() {
+	// Open the root file
+	name := "./data/sonnet-18-jumbled.txt"
+	file, err := os.Open(name)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	// Resolve .yoink commands in the root file
+	txt, err := yoink.Parse(context.Background(), file, name)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(txt)
+}
