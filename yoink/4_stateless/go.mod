1b9cab40dfc69f3b9e4a5064d2f394abfa3abf89
Tedla Brandsema <git@tedla.dev>
2025-07-22 09:56:04 +0200

    Yoink example 4_stateless

---
+++ yoink/4_stateless/go.mod
@@ -0,0 +1,3 @@
+module github.com/tedla-brandsema/examples/yoink/4_stateless
+
+go 1.24.4
---
+++ yoink/4_stateless/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/tedla-brandsema/yoink"
+	"os"
+	"strings"
+)
+
+func HelloParser(sourceFile string, sourceLine int, cmd string) (string, error) {
+	// Default subject of the greeting
+	subject := fmt.Sprintf("from %s", sourceFile)
+
+	// Split the command into its parts where the first part is always the command associated with this parser.
+	// In this case .hello
+	parts := strings.Fields(cmd)
+	if len(parts) > 1 {
+		// Substitute the default subject with the arguments provided
+		subject = strings.Join(parts[1:], " ")
+	}
+
+	// Return the greeting. Starting with the line number, followed by the greeting to our subject.
+	return fmt.Sprintf("%d. Hello, %s!", sourceLine, subject), nil
+}
+
+func main() {
+	// Register the HelloParser, which is a ParserFunc
+	yoink.RegisterParserFunc("hello", HelloParser)
+
+	// Open the root file
+	name := "./data/hello.txt"
+	file, err := os.Open(name)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	// Resolve .yoink commands in the root file
+	txt, err := yoink.Parse(context.Background(), file, name)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(txt)
+
+}
